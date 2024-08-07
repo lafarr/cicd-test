68ecf3993ee05f26480839a17d336f91434592db
James LaFarr <jlafarr99@gmail.com>
2024-08-07 16:24:05 -0500

    Boop

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, CI-CD!")
+        System.out.println("Hello, CI-CD!");
+        long l = ~0;
+        char c = (char) l;
     }
 }
