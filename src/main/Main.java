13b304269aff9246d174048fcbbe824e90d08433
James LaFarr <jlafarr99@gmail.com>
2024-08-07 20:07:02 -0500

    Hopefully this works...

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, CI-CD!")
+        System.out.println("Hello, CI-CD!");
         long l = ~0;
         char c = (char) l;
     }
